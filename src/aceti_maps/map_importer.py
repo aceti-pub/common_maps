3e04529f168c52de207cc1d236e5bf9ace3d1650
AloePacci <bfbdj92@gmail.com>
2025-05-20 10:32:31 +0200

    next approximation

--- src/aceti_maps/map_importer.py
+++ src/aceti_maps/map_importer.py
@@ -1,16 +1,25 @@
 import numpy as np
 import os
+import sys
+if sys.version_info < (3, 9):
+    # importlib.resources either doesn't exist or lacks the files() function, so use the PyPI version:
+    import importlib_resources
+else:
+    # importlib.resources has files(), so use that:
+    import importlib.resources as importlib_resources
+
 def map_importer(map_name: str):
     """
     Import a map from a file and return the base matrix and lat/lon map.
-
     Args:
         map_name (str): The name of the map to import.
-
     Returns:
         tuple: A tuple containing the base matrix and lat/lon map.
     """
-    # Load the base matrix
+    pkg = importlib_resources.files("aceti_maps")
+    if os.path.exists(pkg.joinpath("maps", f"{map_name}mask.npy")):
+        print(f"Loading map {map_name} from repo directory")
+
     base_matrix = np.load(os.path.join("maps", f"{map_name}.npy"))
 
     # Load the lat/lon map
